3da903111f13f577d5d1a6c5322c55f10f0ff099
Oskar Linke <oskarlinke@Oskars-MacBook-Pro.local>
2026-05-24 13:16:25 +0200

    fixes

--- supply-chain.py
+++ supply-chain.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 class SupplyChain: 
-    def __init__(self, n = 10, h = 1, p = 2, k = 3, gamma = 0.9): 
+    def __init__(self, n = 10, h = 1, p = 2, k = 3, gamma = 0.9) -> None: 
         self.n = n #Amount of goods we can buy. Defines both action and state space 
         self.h = h #Holding cost weight
         self.p = p #Lost sales cost weight 
@@ -10,18 +10,17 @@ class SupplyChain:
         self.t = 0 #Time
         self.S = 0 #State 
 
-    def cost(self, s, a, dt):
+    def cost(self, s, a, dt) -> float:
         cost = self.k if a > 0 else 0 
-        print(cost)
         holding_cost = self.h*(s+a-dt) 
         lost_sales_cost = self.p*(dt-s-a) 
 
         cost += holding_cost if holding_cost > 0 else 0
         cost += lost_sales_cost if lost_sales_cost > 0 else 0
 
-        return cost 
+        return cost * self.gamma**self.t
 
-    def step(self, a): 
+    def step(self, a) -> tuple[int, float]: 
         print(f"{a} items are ordered")
         dt = np.random.randint(0,self.n) 
         print(f"{dt} goods are requested by the market")
@@ -31,11 +30,20 @@ class SupplyChain:
         if self.S < 0: 
             self.S = 0 
         print(f"New state is {self.S}")
-        return cost 
+        self.t += 1 
+        return (self.S, cost)
+
+    def action_space(self) -> int: 
+        return self.n - self.S  
+
+    def reset(self) -> None: 
+        self.t = 0 
+        self.S = 0 
+
+def random_action(env: SupplyChain): 
+    return np.random.randint(env.action_space())
 
 if __name__ == "__main__": 
     env = SupplyChain() 
-
-    env.step(5) 
-    env.step(5) 
-    env.step(5)
+    for i in range(5): 
+        env.step(random_action(env))
